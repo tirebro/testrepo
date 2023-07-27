a3467e6b3755549f170a6e62d82f9456bc0aeb73
Vijay Patel <vpatel75040@gmail.com>
2023-07-26 21:10:57 -0500

    added code to app.js and index

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-// this is the server app 
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index2.html'));
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
